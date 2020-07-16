5bf1dc0deb228a00b562cc3578a55106c4753cfe
Ekagra Midha <ekagra99@yahoo.com>
2020-07-16 11:00:32 +0530

    wk Local Commit 01

---
+++ precache-manifest.06c984f6b735deb8ca4868ae72b3021a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da862b081b163b64afe1fac5e3289dd3",
+    "url": "/wk/index.html"
+  },
+  {
+    "revision": "5b752615832db051064b",
+    "url": "/wk/static/css/2.8a102200.chunk.css"
+  },
+  {
+    "revision": "fa6c645dd3eefd341fc7",
+    "url": "/wk/static/css/main.c93002fa.chunk.css"
+  },
+  {
+    "revision": "5b752615832db051064b",
+    "url": "/wk/static/js/2.042f2f15.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/wk/static/js/2.042f2f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6c645dd3eefd341fc7",
+    "url": "/wk/static/js/main.9b2f7f61.chunk.js"
+  },
+  {
+    "revision": "d8e553d21223a8ad03d3",
+    "url": "/wk/static/js/runtime-main.bb1c3592.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/wk/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/wk/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/wk/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/wk/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/wk/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "11d3915cc2651be857aad721442b62ca",
+    "url": "/wk/static/media/wkLogo.11d3915c.jpg"
+  },
+  {
+    "revision": "40ebc212f78d221ea84b55f5487804a1",
+    "url": "/wk/static/media/wkLogoTransparent.40ebc212.png"
+  }
+]);
